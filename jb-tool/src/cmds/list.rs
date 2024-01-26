bd301b63ce5e7508eab44dbb3f5a6c6e4e7b1e67
Brewen Couaran <45310490+brewcoua@users.noreply.github.com>
2024-01-26 20:43:32 +0100

    feat(list): add checkmark and cross to show link state next to tools

--- jb-tool/src/cmds/list.rs
+++ jb-tool/src/cmds/list.rs
@@ -21,18 +21,23 @@ pub(crate) fn dispatch(args: &clap::ArgMatches) -> Result<()> {
     let installed_tools = Tool::list(directory)?;
 
     println!(
-        "{:<20} {:<20} {:<20}",
+        "{:<1} {:<20} {:<20} {:<20}",
+        " ",
         "Tool".bold().underline(),
         "Version".bold().underline(),
         "Release Type".bold().underline(),
     );
+
+    let checkmark = char::from_u32(0x2714).unwrap().to_string();
+    let cross = char::from_u32(0x2718).unwrap().to_string();
 
     for tool in installed_tools {
         let version = tool.version().unwrap_or(ReleaseVersion::default());
 
         if tool.is_linked() {
             println!(
-                "{:<20} {:<20} {:<20}",
+                "{:<1} {:<20} {:<20} {:<20}",
+                checkmark.green(),
                 tool.kind().pretty(),
                 version.to_string(),
                 version.release.pretty(),
@@ -41,7 +46,8 @@ pub(crate) fn dispatch(args: &clap::ArgMatches) -> Result<()> {
             println!(
                 "{}",
                 format!(
-                    "{:<20} {:<20} {:<20}",
+                    "{:<1} {:<20} {:<20} {:<20}",
+                    cross.red(),
                     tool.kind().pretty(),
                     version.to_string(),
                     version.release.pretty(),
