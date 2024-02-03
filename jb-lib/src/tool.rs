a0a7e53b0343f4e5d1f382bca64f282251bed55e
Brewen Couaran <45310490+brewcoua@users.noreply.github.com>
2024-02-03 22:23:12 +0100

    fix(install): clean up temporary files even if install fails

--- jb-lib/src/tool.rs
+++ jb-lib/src/tool.rs
@@ -513,18 +513,29 @@ impl Tool {
 
         let download_path = temp_folder.join(archive_name);
 
-        file::download(&download.link, &download_path, Some(download.size))?;
-        file::download(&download.checksum_link, &download_path.with_extension("sha256"), None)?;
+        let install_process = || {
+            file::download(&download.link, &download_path, Some(download.size))?;
+            file::download(&download.checksum_link, &download_path.with_extension("sha256"), None)?;
 
-        // Verify the checksum
-        file::checksum(&download_path.with_extension("sha256"), &download_path)?;
+            // Verify the checksum
+            file::checksum(&download_path.with_extension("sha256"), &download_path)?;
 
-        std::fs::create_dir_all(&tool_dir)
-            .with_context(|| format!("Failed to create tool directory {}", tool_dir.display()))?;
-        std::fs::create_dir_all(&icons_dir)
-            .with_context(|| format!("Failed to create icons directory {}", icons_dir.display()))?;
+            std::fs::create_dir_all(&tool_dir)
+                .with_context(|| format!("Failed to create tool directory {}", tool_dir.display()))?;
+            std::fs::create_dir_all(&icons_dir)
+                .with_context(|| format!("Failed to create icons directory {}", icons_dir.display()))?;
 
-        file::extract_archive(&download_path, &tool_dir, 1)?;
+            file::extract_archive(&download_path, &tool_dir, 1)?;
+
+            // Symlink the tool
+            self.link()?;
+            tracing::debug!("Symlinked {self} to {}", tool_dir.display());
+
+            Ok(())
+        };
+
+        // Make sure to clean up even if the installation fails (to avoid leaving temporary files behind)
+        let output = install_process();
 
         // Clean up the temporary directory
         std::fs::remove_dir_all(&temp_folder).with_context(|| {
@@ -535,11 +546,7 @@ impl Tool {
         })?;
         tracing::debug!("Removed temporary directory {}", temp_folder.display());
 
-        // Symlink the tool
-        self.link()?;
-        tracing::debug!("Symlinked {self} to {}", tool_dir.display());
-
-        Ok(())
+        output
     }
 
     /// Uninstall the tool.
