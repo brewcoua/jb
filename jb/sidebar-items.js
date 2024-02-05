422909115e8db63d07bf2ed195f15c36bf4ca7c5
brewcoua <brewcoua@users.noreply.github.com>
2024-02-05 14:34:40 +0000

    deploy: d3402f7c4b525bf0e24a42d53c77bcbbde558a44

--- jb/sidebar-items.js
+++ jb/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["bail","bail_with","error","error_with"],"mod":["api","env","error","tool"]};
+window.SIDEBAR_ITEMS = {"macro":["bail","bail_with","error","error_with"],"mod":["api","env","error","tool","util"]};
